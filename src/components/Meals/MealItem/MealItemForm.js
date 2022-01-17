6043c60cc0c01523f103287af154daf7fbd98e83
Deepak <iqueenpreeti@mail.com>
2022-01-17 20:12:25 +0530

    First Commit

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,37 @@
+import { useRef  } from "react";
+
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = () => {
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount; 
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add </button>
+    </form>
+  );
+};
+
+
+export default MealItemForm;
+
